1381050a423f0b47ef97d6a3277c71dbfaad8363
Natasha Buckham <natashaisobel@gmail.com>
2024-07-25 11:00:51 +0100

    Adds Post component and passes data down as props

--- frontend/src/Post/Post.tsx
+++ frontend/src/Post/Post.tsx
@@ -1,19 +1,18 @@
-// interface PostProps {
-//     username: string,
-//     imageurl: string,
-//     message: string,
-//     createdAt: string,
-//     likedBy: UserProps[],
-// }
+interface PostProps {
+    username: string,
+    imageUrl: string,
+    message: string,
+    createdAt: string,
+}
 
-export default function Post(postData) {
+export default function Post(props: PostProps) {
     
     return (
         <div>
-            <h2>Username: {postData.postedBy.username}</h2>
-            <img src={postData.imageUrl} alt="User-uploaded image" />
-            <p>{postData.message}</p>
-            <p>{postData.createdAt}</p>
+            <h2>Username: {props.username}</h2>
+            <img src={props.imageUrl} alt="User-uploaded image" />
+            <p>{props.message}</p>
+            <p>{props.createdAt}</p>
         </div>
     )
 }
--- frontend/src/PostList/PostList.tsx
+++ frontend/src/PostList/PostList.tsx
@@ -3,27 +3,6 @@ import Post from "../Post/Post";
 
 export default function PostList() {
     const [ posts, setPosts ] = useState(null);
-
-    // interface PostUserModel {
-    //     id: number;
-    //     name: string;
-    //     username: string;
-    //     email: string;
-    // }
-
-    // interface PostModel {
-    //     id: number;
-    //     message: string;
-    //     imageUrl: string;
-    //     createdAt: moment.Moment;
-    //     postedBy: PostUserModel;
-    //     likedBy: PostUserModel[];
-    //     dislikedBy: PostUserModel[];
-    // }
-    // useState<PostModel[] | null>(null)
-    
-    
-    // {[{post 1}, {post 2}, {post 3}]}
     
     useEffect(() => {
         fetch("http://localhost:3001/posts")
@@ -36,10 +15,12 @@ export default function PostList() {
             {
               posts && posts.map((post) => (
                 <div>
-                    <h2>Username: {post.postedBy.username}</h2>
-                    <img src={post.imageUrl} alt="User-uploaded image" /> 
-                    <p>{post.message}</p>
-                    <p>{post.createdAt}</p>
+                    <Post 
+                    username={post.postedBy.username} 
+                    imageUrl={post.imageUrl} 
+                    message={post.message} 
+                    createdAt={post.createdAt}
+                    />
                 </div>
               ))
             }
